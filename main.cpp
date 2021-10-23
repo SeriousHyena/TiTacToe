7fabb87030b319efd4e75ae4bf4e9d779e8f58bb
SeriousHyena <emackiii@gmail.com>
2021-10-23 08:19:51 -0700

    added runGame function

--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
+
+/*
+* Tic-Tac-Toe project
+* Author: Ernest Mack
+* Date: 10/23/21
+* 
+* 
+*/
+
 #include <iostream>
 #include <array>
 #include <string>
 
-
 using namespace std;
 
-//declare functions here
+//declare constants here
+const int ROWS = 3;
+const int COLS = 3;
 
+//declare functions here
+void runGame();
 
 
 int main()
 {
-
+	runGame();
 	return 0;
 }
 
 //place functions here
+void runGame()
+{
+	
+}
