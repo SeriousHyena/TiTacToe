96ac5225cdda07af4db80cd3369f7bbe8b432e8f
SeriousHyena <emackiii@gmail.com>
2021-10-24 13:02:03 -0700

    improved board layout

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 * Tic-Tac-Toe project
 * Author: Ernest Mack
 * Date: 10/23/21
-* 
+* D E S C R I P T I O N
+* -------------------------------------------------------------------------------------------------------------------------------
+* the "void boardArray" function returns nothing and constructs the game board
 * 
 */
 
@@ -16,39 +18,45 @@ using namespace std;
 //declare constants here
 string row = "-";									//a container for the string character in the row part of the array
 string col = "|";									//a container for the string character in the column part of the array
+const int ROW = 3;
+const int COL = 5;
+string userTurn;
 
 
 //declare arrays here
-string board[3][5]{
-	{"---", "|", "---","|", "---",},
-	{"---", "|", "---","|", "---"},					//construct the array to build the game board				
-	{"---", "|", "---","|", "---"}
+string board[4][5]{
+	{"___", "|", "___","|", "___",},
+	{"___", "|", "___","|", "___"},					//construct the array to build the game board				
+	{"___", "|", "___","|", "___"},
+	{"   ", "|", "   ","|", "   "}
+	
 };
-
-
-
 
 //declare functions here
 void boardArray(string row, string col);			//construct the board using 2-d array
+void getUserInput(string userTurn, string boardArray[ROW][COL]);
 
 string fum = "";
-
 
 int main()
 {
 	
-	void boardArray(string row, string col);
+	void getUserInput(string userTurn, string boardArray[ROW][COL]);
+	void boardArray(string row, string col);		//call function to build the game board
 	boardArray(row, col);
+
+
+	
+
 
 	
 	return 0;
 }
 
 //place functions here
-
-void boardArray(string row, string col)					//simple function to print out the game board
+void boardArray(string row, string col)				//simple function to print out the game board
 {
-	for (int row = 0; row < 3; row ++)
+	for (int row = 0; row < 4; row ++)
 	{
 		for (int col = 0; col < 5; col ++)
 		{
@@ -56,4 +64,10 @@ void boardArray(string row, string col)					//simple function to print out the g
 		}
 		cout << endl;
 	}
+}
+
+void getUserInput(string userTurn, string boardArray[ROW][COL])
+{
+	cout << "Enter a move on the board in the form of row,col: ";
+
 }
